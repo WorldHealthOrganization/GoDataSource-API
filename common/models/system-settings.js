9804d4c8fdd4a716f57db9885669ad524cfbe7d7
atoader-clarisoft <adrian.toader@clarisoft.com>
2018-04-04 16:39:32 +0300

    Initial model description

--- common/models/system-settings.js
+++ common/models/system-settings.js
@@ -1,5 +1,31 @@
 'use strict';
 
-module.exports = function(Systemsettings) {
+const app = require('../../server/server');
 
+module.exports = function(SystemSettings) {
+  app.utils.remote.disableRemoteMethods(SystemSettings, [
+    'create',
+    'findById',
+    'deleteById',
+    'prototype.patchAttributes',
+    'count',
+    'find'
+  ]);
+
+  /**
+   * Retrieve System Settings
+   * @param cb
+   */
+  SystemSettings.getSystemSettings = function (cb) {
+    cb();
+  };
+
+  /**
+   * Modify System Settings
+   * @param data
+   * @param cb
+   */
+  SystemSettings.updateSystemSettings = function (data, cb) {
+    cb(null, data);
+  };
 };
