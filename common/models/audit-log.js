f671a22ec1bf90904f27a252fa754fec5b1eb0ba
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-04-12 17:55:05 +0300

    GD-FD_1: Authentication/Roles and Permissions Support

--- common/models/audit-log.js
+++ common/models/audit-log.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const app = require('../../server/server');
+
 module.exports = function(AuditLog) {
 
   AuditLog.actions = {
@@ -7,5 +9,8 @@ module.exports = function(AuditLog) {
     modified: 'Record Modified',
     removed: 'Record Removed'
   };
+
+  // Audit Log has only read-only endpoints
+  app.utils.remote.disableRemoteMethods(AuditLog, ['create', 'prototype.patchAttributes', 'findById', 'deleteById']);
 
 };
