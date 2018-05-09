68653e5406a1bf2b1bb5fb054fc6d2d0856da3c8
atoader-clarisoft <adrian.toader@clarisoft.com>
2018-05-09 17:43:29 +0300

    [GD-FD_2] - ST5

--- common/models/location.js
+++ common/models/location.js
@@ -8,9 +8,9 @@ module.exports = function (Location) {
   Location.hasController = false;
 
   /**
-   * Get sub-locations for a list of locations
-   * @param parentLocations
-   * @param allLocations
+   * Get sub-locations for a list of locations. Result is an array of location IDs
+   * @param parentLocations Array of location Ids for which to get the sublocations
+   * @param allLocations Array on which to add the result; Must be an array of location IDs
    * @param callback
    */
   Location.getSubLocations = function (parentLocations, allLocations, callback) {
@@ -52,5 +52,62 @@ module.exports = function (Location) {
         }
       })
       .catch(callback);
-  }
+  };
+
+  /**
+   * Get sub-locations for a list of locations. Result is an array of location models
+   * @param parentLocationsIds Array of location Ids for which to get the sublocations
+   * @param allLocations Array on which to add the result; Must be an array of location models
+   * @param callback
+   */
+  Location.getSubLocationsWithDetails = function (parentLocationsIds, allLocations, callback) {
+    // get the location IDs from the allLocations array
+    let allLocationsIds = allLocations.map(location => location.id);
+
+    // get IDs of the parentLocations that are not in the allLocations array
+    let notRetrievedParentLocationsIds = parentLocationsIds.filter(locationId => allLocationsIds.indexOf(locationId) === -1);
+
+    // find not already retrieved parent locations as well as sublocations
+    Location
+      .find({
+        where: {
+          or: [{
+            id: {
+              in: notRetrievedParentLocationsIds
+            }
+          },
+          {
+            parentLocationId: {
+              in: parentLocationsIds
+            }
+          }]
+        }
+      })
+      .then(function (locations) {
+        // if children locations found
+        if (locations.length) {
+          // store them
+          let foundLocationsIds = [];
+          locations.forEach(function (location) {
+            // check if the retrieved location is not a searched parent location
+            if(notRetrievedParentLocationsIds.indexOf(location.id) === -1) {
+              // sublocation; avoid loops
+              if (allLocationsIds.indexOf(location.id) === -1) {
+                foundLocationsIds.push(location.id);
+              } else {
+                app.logger.warn(`Detected loop in location hierarchy: location with id "${location.id}" is set as a child location for a location that is lower the hierarchy. Scanned locations ids: ${allLocationsIds.join(', ')}`)
+              }
+            }
+          });
+          // consolidate them in the locations list
+          allLocations = allLocations.concat(locations);
+          // scan their children
+          Location.getSubLocationsWithDetails(foundLocationsIds, allLocations, callback);
+        } else {
+          // no more locations found, stop here
+          callback(null, allLocations);
+        }
+      })
+      .catch(callback);
+  };
 };
