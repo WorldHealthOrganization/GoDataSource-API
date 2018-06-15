a79357cb2ea6e0511ce8c47f7e7759ee3ab8df70
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-06-15 16:48:31 +0300

    [GD-FD_6] - ST28 As a Go.Data user, I want to filter contacts by multiple criteria. I want to search by name. I want to search by occupation. I want to search by date ranges. I want to search by age ranges. I want to search by place. I want to search by case they (contacts) are exposed to.

--- components/searchByRelationProperty.js
+++ components/searchByRelationProperty.js
@@ -2,18 +2,12 @@
 
 const _ = require('lodash');
 
-/**
- * Attach behavior or a Model remote
- * @param Model
- * @param remote
- */
-function attachOnRemote(Model, remote) {
 
-  /**
-   * Filter results by related model properties
-   * Applying a filter on related model properties Loopback only filters the related model inclusion in the result
-   * This hook removes from the result the items that don't contain the filtered related model if the relation scope.filterParent flag is sent and true
-   * eg filter: {
+/**
+ * Filter results by related model properties
+ * Applying a filter on related model properties Loopback only filters the related model inclusion in the result
+ * This hook removes from the result the items that don't contain the filtered related model if the relation scope.filterParent flag is sent and true
+ * eg filter: {
         "include": [
           {
             "relation": "role",
@@ -27,43 +21,87 @@ function attachOnRemote(Model, remote) {
           }
         ]
       }
-   * Based on the above example the hook removes the entries that don't contain the embedded role model (this is already filtered by Loopback)
+ * Based on the above example the hook removes the entries that don't contain the embedded role model (this is already filtered by Loopback)
+ */
+
+
+/**
+ * Search recursively through the nested relations, based on 'filterParent' parameter, decide if a record should be included when the relation is empty
+ * @param model
+ * @param filter
+ * @return {*}
+ */
+function deepSearchByRelationPropertyOnModel(model, filter) {
+  // always work with JSON to be able to traverse relation data
+  if (typeof model.toJSON === 'function') {
+    model = model.toJSON();
+  }
+  // check if there's a filter
+  if (filter.include) {
+    // always use arrays (normalize filters)
+    if (!Array.isArray(filter.include)) {
+      filter.include = [filter.include];
+    }
+    filter.include.forEach(function (include) {
+      // if the include filter has a scope
+      if (include.relation && include.scope) {
+        // perform the search (apply filtering)
+        model[include.relation] = deepSearchByRelationProperty(model[include.relation], include.scope);
+        // if the parent needs to be filtered and the relation was not found, clear the parent (set null)
+        if (include.scope.filterParent && (!model[include.relation] || (Array.isArray(model[include.relation]) && !model[include.relation].length))) {
+          model = null;
+        }
+      }
+    });
+  }
+  return model;
+}
+
+/**
+ * Search recursively through the nested relations of a model or list of models, based on 'filterParent' parameter,
+ * decide if a record should be included when the relation is empty
+ * @param resource
+ * @param filter
+ * @return {*}
+ */
+function deepSearchByRelationProperty(resource, filter) {
+  let result;
+  // array of resources
+  if (Array.isArray(resource)) {
+    result = [];
+    // handle each resource separately
+    resource.forEach(function (singleResource) {
+      // perform the search (apply filtering)
+      let res = deepSearchByRelationPropertyOnModel(singleResource, filter);
+      if (res) {
+        result.push(res);
+      }
+    })
+  } else {
+    // single element, perform search
+    result = deepSearchByRelationPropertyOnModel(resource, filter);
+  }
+  return result;
+}
+
+/**
+ * Attach behavior or a Model remote
+ * @param Model
+ * @param remote
+ */
+function attachOnRemote(Model, remote) {
+  /**
+   * Attach the behavior on specified remote
    */
-  Model.afterRemote(remote, function (context, models, next) {
+  Model.afterRemote(remote, function (context, model, next) {
     // check for include filter
     let includeFilter = _.get(context, 'args.filter.include', []);
     // also include custom relations
     includeFilter = includeFilter.concat(_.get(context, 'args.filter.includeCustom', []));
     // normalize the include filter as an array
     includeFilter = Array.isArray(includeFilter) ? includeFilter : [includeFilter];
-
-    // get from the include filter the properties that need to be checked for each item in the result; these are the 'relation' values from each item in the includeFilter that has a scope.where clause
-    let props = [];
-    props = includeFilter.map(function (rel) {
-      return rel.scope && rel.scope.filterParent ? rel.relation : null;
-    }).filter(function (rel) {
-      return rel !== null;
-    });
-
-    // initialize the new results list
-    let results = [];
-
-    // check is there are properties to be checked in each model instance
-    if (props.length) {
-      // get from the models list only the ones that contain all the props
-      results = models.filter(function (model) {
-        model = model.toJSON();
-        return props.filter(function (prop) {
-          return !!model[prop] && (!Array.isArray(model[prop]) || model[prop].length);
-        }).length === props.length;
-      });
-    } else {
-      results = models;
-    }
-
     // overwrite the found results
-    context.result = results;
-
+    context.result = deepSearchByRelationProperty(context.result, {include: includeFilter});
     next();
   });
 }
@@ -81,5 +119,6 @@ module.exports = {
     remotes.forEach(function (remote) {
       attachOnRemote(model, remote);
     });
-  }
+  },
+  deepSearchByRelationProperty: deepSearchByRelationProperty
 };
