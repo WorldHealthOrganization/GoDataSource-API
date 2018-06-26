afddd62e5a969b13e6e27c15b4b5037781e49751
stoianmarius <stoianmarius@live.com>
2018-06-26 17:37:37 +0300

    CaseContactSupport_FD - Follow-ups stories

--- common/models/team.js
+++ common/models/team.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// dependencies
-const app = require('../../server/server');
-
 module.exports = function (Team) {
 
   // define a list of custom (non-loopback-supported) relations
@@ -17,18 +14,5 @@ module.exports = function (Team) {
       model: 'location',
       foreignKey: 'locationIds'
     }
-  };
-
-  // initialize model helpers
-  Team.helpers = {};
-
-  // get matching teams for a list of location/sub locations
-  Team.helpers.getTeamsByLocationId = function (locationId, callback) {
-    // retrieve all teams
-    app.models.team.find()
-      .then((teams) => {
-
-      });
-    //return app.models.location.getSubLocations(locationId, [], callback);
   };
 };
