fcd6322e937174af25b67a0b682f6c39cfa439d9
stoianmarius <stoianmarius@live.com>
2018-07-05 14:25:32 +0300

    WGD-349

--- components/helpers.js
+++ components/helpers.js
@@ -12,6 +12,30 @@ const getUTCDate = function (date) {
   return date ? moment(date).utc().startOf('day') : moment.utc().startOf('day');
 };
 
+/**
+ * Convert filter date attributes from string to date
+ * @param obj
+ */
+const convertPropsToDate = function (obj) {
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      if (typeof obj[prop] == 'object' && obj[prop] !== null) {
+        convertPropsToDate(obj[prop]);
+      } else {
+        // we're only looking for strings properties to convert
+        if (typeof obj[prop] === 'string') {
+          // try to convert the string value to date, if valid, replace the old value
+          let convertedDate = moment(obj[prop]);
+          if (convertedDate.isValid()) {
+            obj[prop] = convertedDate.toDate();
+          }
+        }
+      }
+    }
+  }
+};
+
 module.exports = {
-  getUTCDate: getUTCDate
+  getUTCDate: getUTCDate,
+  convertPropsToDate: convertPropsToDate
 };
