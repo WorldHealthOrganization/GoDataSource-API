d054c91f7ad4a0b343654d5810c3d85b4adc3fd9
atoader-clarisoft <adrian.toader@clarisoft.com>
2018-07-12 18:17:34 +0300

    WGD-347 ST30: As a Go.Data user I want to be able to view the epi curve on the dashboard: case counts over time stratified by classification

--- components/helpers.js
+++ components/helpers.js
@@ -4,12 +4,25 @@
 const moment = require('moment');
 
 /**
- * Convert a Date object into moment UTC date and reset time to start of the day
+ * Convert a Date object into moment UTC date; reset day to the given day and reset time to start of the day
  * If no date is given, the current datetime is returned
  * @param date
+ * @param dayOfWeek If not sent the date will not be changed
  */
-const getUTCDate = function (date) {
-  return date ? moment(date).utc().startOf('day') : moment.utc().startOf('day');
+const getUTCDate = function (date, dayOfWeek) {
+  let momentDate = date ? moment(date).utc().startOf('day') : moment.utc().startOf('day');
+  return !dayOfWeek ? momentDate : momentDate.day(dayOfWeek);
+};
+
+/**
+ * Convert a Date object into moment UTC date; reset day to the given day and reset time to end of the day
+ * If no date is given, the current datetime is returned
+ * @param date
+ * @param dayOfWeek If not sent the date will not be changed
+ */
+const getUTCDateEndOfDay = function (date, dayOfWeek) {
+  let momentDate = date ? moment(date).utc().endOf('day') : moment.utc().endOf('day');
+  return !dayOfWeek ? momentDate : momentDate.day(dayOfWeek);
 };
 
 /**
@@ -23,5 +36,6 @@ const getAsciiString = function(string) {
 
 module.exports = {
   getUTCDate: getUTCDate,
+  getUTCDateEndOfDay: getUTCDateEndOfDay,
   getAsciiString: getAsciiString
 };
