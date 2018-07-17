422c4687bee1f68e207e35277c1def6c201bc577
stoianmarius <stoianmarius@live.com>
2018-07-17 16:35:49 +0300

    WGD-282 ST2: As a Go.Data user, I want to manage a list of client applications that can connect to current node/hub

--- common/controllers/sync.js
+++ common/controllers/sync.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+const formidable = require('formidable');
 const app = require('../../server/server');
 
 module.exports = function (Sync) {
@@ -13,11 +15,53 @@ module.exports = function (Sync) {
     filter = filter || {};
     filter.where = filter.where || {};
 
-    Sync.exportDatabase(filter, [], (err, fileName) => {
+    Sync.exportDatabase(
+      filter,
+      [
+        'systemSettings',
+        'template',
+        'icon',
+        'helpCategory'
+      ],
+      [],
+      (err, fileName) => {
+        if (err) {
+          return done(err);
+        }
+        return done(null, fs.createReadStream(fileName), 'application/octet-stream');
+    });
+  };
+
+  /**
+   * Synchronize database based on a given snapshot archive containing matching collections
+   * @param req
+   * @param snapshot Database snapshot .tar.gz archive
+   * @param done
+   */
+  Sync.importDatabaseSnapshot = function (req, snapshot, done) {
+    const buildError = app.utils.apiError.getError;
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
       if (err) {
-        return done(err);
+        return done(error);
       }
-      return done(null, fileName);
+
+      // validates snapshot archive
+      if (!files.snapshot) {
+        // send back the error
+        return done(buildError('MISSING_REQUIRED_PROPERTY', {
+          model: Sync.modelName,
+          properties: 'snapshot'
+        }));
+      }
+      // validate that the archive is a .tar.gz
+      if (files.snapshot.type !== 'application/gzip') {
+        return done(buildError('INVALID_SNAPSHOT_FILE'));
+      }
+
+      // extract the archive to the temporary directory
+      Sync.extractDatabaseArchive(files.snapshot.path, done);
     });
   };
 };
--- common/models/sync.js
+++ common/models/sync.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const zlib = require('zlib');
+const tar = require('tar');
 const tmp = require('tmp');
 const async = require('async');
 const app = require('../../server/server');
+const fs = require('fs');
 
 module.exports = function (Sync) {
   Sync.hasController = true;
@@ -31,49 +32,86 @@ module.exports = function (Sync) {
 
   /**
    * Helper function used to export the database's collections
-   * You may choose which collections should be excluded
-   * It supports a custom filter { fromDate: Date } to only retrieve records past a given date
-   * It also supports a flag that indicates whether the contents should be compressed
-   * It exports each collection and stores it in a file, to not kill the RAM
+   * The following extra features are available:
+   * - exclude some collections
+   * - collection specific options:
+   *   - encrypt the file
+   *   - exclude some properties
+   * - custom filter { fromDate: Date } to only retrieve records past a given date
    * @param filter
    * @param excludes
+   * @param collectionsOpts
    * @param done
    */
-  Sync.exportDatabase = function (filter = { where: {} }, excludes = [], done) {
+  Sync.exportDatabase = function (filter = { where: {} }, excludes = [], collectionsOpts = [], done) {
+    // defensive checks
+    collectionsOpts.map((collectionOpts) => {
+      collectionOpts.excludes = collectionOpts.excludes || [];
+      collectionOpts.shouldEncrypt = collectionOpts.shouldEncrypt || false;
+    });
+
     // cache reference to mongodb connection
     let connection = app.dataSources.mongoDb.connector;
 
     // parse from date filter
-    let customFilter = { where: {}};
+    let customFilter = null;
     if (filter.where.hasOwnProperty('fromDate')) {
       // doing this because createdAt and updatedAt are equal when a record is created
-      customFilter.where.updatedAt = { $gte: filter.where.fromDate };
+      customFilter = {
+        where: {
+          updatedAt: {
+            $gte: filter.where.fromDate
+          }
+        }
+      };
     }
 
     // create a copy of the collections map and exclude the ones from the list of excludes (if any)
     let collections = Object.assign({}, collectionsMap);
     Object.keys(collections).forEach((collectionName) => {
-      if (excludes.indexOf(collectionName)) {
-        collections[collectionName] = null;
+      if (excludes.indexOf(collectionName) !== -1) {
+        delete collections[collectionName];
       }
     });
 
-    // create a temporary directory to store the database files and compressed dump
-    let dirName = tmp.tmpNameSync();
+    // create a temporary directory to store the database files
+    // it always created the folder in the system temporary directory
+    let tmpDir = tmp.dirSync();
+    let tmpDirName = tmpDir.name;
 
     return async
       .series(
         Object.keys(collections).map((collectionName) => {
           return (callback) => {
+            // look up for any specific options
+            let collectionOpts = collectionsOpts.filter((item) => item.name === collectionName)[0];
+
+            // check if properties should be excluded
+            let excludes = {};
+
+            if (collectionOpts && collectionOpts.excludes.length) {
+              Object.keys(collectionOpts.excludes).forEach((prop) => {
+                excludes[prop] = 0;
+              });
+            }
+
             return connection
               .collection(collections[collectionName])
-              .find(customFilter, (err, results) => {
+              .find(customFilter, excludes, (err, result) => {
                 if (err) {
                   return callback(err);
                 }
 
-                // create a file with collection name as file name, containing results
+                // retrieve
+                result.toArray((err, records) => {
+                  if (err) {
+                    return callback(err);
+                  }
 
+                  // Note: Encryption is not supported yet
+                  // create a file with collection name as file name, containing results
+                  fs.writeFile(`${tmpDirName}/${collectionName}.json`, JSON.stringify(records), callback);
+                });
               });
           };
         }),
@@ -81,10 +119,117 @@ module.exports = function (Sync) {
           if (err) {
             return done(err);
           }
-          // compress all collection files
 
-          return done();
+          // archive file name
+          let archiveName = `${tmpDirName}/db_snapshot_${Date.now()}.tar.gz`;
+
+          // retrieve all files in the temporary directory
+          return fs.readdir(tmpDirName, function (err, filenames) {
+            if (err) {
+              return done(err);
+            }
+
+            // compress all collection files from the tmp dir into .tar file
+            tar
+              .c(
+                {
+                  gzip: true,
+                  file: archiveName,
+                  cwd: tmpDirName
+                },
+                filenames
+              )
+              .then(() => done(null, archiveName));
+          });
         }
       );
   };
+
+  /**
+   * Extract a database snapshot archive to a temporary directory
+   * @param filePath
+   * @param callback
+   */
+  Sync.extractDatabaseArchive = function (filePath, callback) {
+    // create a temporary directory to store the database files
+    // it always created the folder in the system temporary directory
+    let tmpDir = tmp.dirSync();
+    let tmpDirName = tmpDir.name;
+
+    // cache reference to mongodb connection
+    let connection = app.dataSources.mongoDb.connector;
+
+    // create a tar extract stream
+    tar.x(
+      {
+        cwd: tmpDirName,
+        file: filePath
+      },
+      (err) => {
+        if (err) {
+          return callback(err);
+        }
+
+        // retrieve all the extracted files
+        return fs.readdir(tmpDirName, function (err, filenames) {
+          if (err) {
+            return callback(err);
+          }
+
+          // filter files that match a collection name
+          let collectionsFiles = filenames.filter((filename) => {
+            // split filename into 'collection name' and 'extension'
+            filename = filename.split('.');
+            return collectionsMap.hasOwnProperty(filename[0]);
+          });
+
+          // read each file's contents and sync with database
+          return async.parallel(collectionsFiles.map((fileName) => {
+            return (done) => {
+              // split filename into 'collection name' and 'extension'
+              let collectionName = fileName.split('.');
+
+              // cache reference to Loopback's model
+              let model = app.models[collectionsMap[collectionName]];
+
+              fs.readFile(`${tmpDirName}/${fileName}`, (err, data) => {
+                // parse file contents to JavaScript object
+                try {
+                  let collectionRecords = JSON.parse(data);
+
+                  return async.parallel(
+                    collectionRecords.map((collectionRecord) => {
+                      return (done) => {
+                        // check if a record with the given id exists
+                        // if not, create it, otherwise check updatedAt timestamp
+                        // if it's different, then update the record using data from the snapshot
+                        model.findById(collectionRecord._id, (err, record) => {
+                            if (err) {
+                              return done(err);
+                            }
+
+                            if (!record) {
+                              return model.save(collectionRecord, done);
+                            }
+
+                            if (record && record.updatedAt !== collectionRecord.updatedAt) {
+                              return model.updateAttributes(collectionRecord, done);
+                            }
+
+                            return done();
+                          });
+                      };
+                    }),
+                    (err) => callback(err)
+                  );
+                } catch (parseError) {
+                  return done(parseError);
+                }
+              });
+            };
+          }));
+        });
+      }
+    );
+  }
 };
