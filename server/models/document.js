9dcffa233b311b9931ef77e7ebe22821fd01ec5f
stoianmarius <stoianmarius@live.com>
2018-08-06 16:25:33 +0300

    WGD-426 ST33: As a Go.Data user I must be able to deliver a printable version of an empty case investigation form including its three sections for cases, contact and lab

--- server/models/document.js
+++ server/models/document.js
@@ -2,7 +2,7 @@
 
 module.exports = function(Document) {
   Document.fieldLabelsMap = {
-    'type': 'LNG_DOCUMENT_FIELD_LABEL_DOCUMENT_TYPE',
-    'number': 'LNG_DOCUMENT_FIELD_LABEL_DOCUMENT_NUMBER'
+    'type': 'LNG_CASE_FIELD_LABEL_DOCUMENT_TYPE',
+    'number': 'LNG_CASE_FIELD_LABEL_DOCUMENT_NUMBER'
   };
 };
