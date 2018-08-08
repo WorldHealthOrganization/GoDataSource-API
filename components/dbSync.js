0eb348fa48e5e75bc2476c149c7ab01f04746928
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-08-08 12:15:37 +0300

    WGD-501 ST1: As a Go.Data user I want to import data from structured files

--- components/dbSync.js
+++ components/dbSync.js
@@ -87,7 +87,7 @@ const syncRecord = function (logger, model, record, options, done) {
 
       // if record was created from third parties, it might not have updated/created timestamps
       // in this case, just create a new record with new id
-      if (!dbRecord.updatedAt) {
+      if (!record.updatedAt) {
         log('debug', `Record found (id: ${record.id}) but data received is missing updatedAt property, probably comes from external system, creating new record.`);
         delete record.id;
         return model
