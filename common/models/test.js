14a9de92cf872f49d096d4b24330930a14709c14
vlad.rotaru <vlad.rotaru@clarisoft.com>
2018-09-03 19:45:09 +0300

    [WGD-866] Relationships can be created with more than 2 people.
    Created a script that all relationships in the DB have a correct form.

--- common/models/test.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB("go-data");
-
-db.relationship.find({}).forEach((relationship) => {
-    var newPersons = [relationship.persons[0], relationship.persons[relationship.persons.length - 1]];
-    persons = [];
-    db.relationship.updateOne({"_id": relationship._id}, {$set: {persons: newPersons}});
-});
