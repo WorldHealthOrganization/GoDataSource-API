c4a2daa5c0d7710a65d706683a4b19893f4648e7
Stoian Marius <stoianmarius@live.com>
2018-09-04 17:46:29 +0300

    WGD-533 ST3: As a Go.Data user I want to manually trigger data backup

--- server/boot/routines.js
+++ server/boot/routines.js
@@ -1,86 +1,168 @@
 'use strict';
 
 // requires
-const cron = require('node-cron');
+const moment = require('moment');
 const async = require('async');
 const fs = require('fs');
+const path = require('path');
 
-module.exports = function (app) {
-  // build list of routines to be executed
-  async.series([
-    (done) => {
-      // retrieve system settings and get backup timings
-      app.models.systemSettings
-        .findOne()
-        .then((systemSettings) => {
-          if (systemSettings.dataBackup) {
-            // cache backup settings
-            let backupSettings = systemSettings.dataBackup;
-
-            // backup interval is in hours
-            let backupIntervalHours = backupSettings.backupInterval;
-
-            // retention interval is in days
-            let retentionIntervalDays = backupSettings.dataRetentionInterval;
-
-            // make sure the backup location is ok, otherwise don't schedule the job
-            fs.access(backupSettings.location, fs.F_OK, (accessError) => {
-              if (accessError) {
-                app.logger.error(`Configured backup location: ${backupSettings.location} is not OK. ${accessError}`);
-                return done(accessError);
-              }
-
-              // load the backup module
-              let backup = require('../../components/backup');
-
-              // if intervals are 0, then don't schedule
-              if (backupIntervalHours >= 1) {
-                app.logger.debug('Scheduled automatic backup');
-                // schedule the backup cron
-                let backupCron = cron.schedule(`* * ${backupIntervalHours} * * *`, function () {
-                  const backupModel = app.models.backup;
-
-                  // create new backup record with pending status
-                  backupModel
-                    .create({
-                      date: Date.now(),
-                      modules: backupSettings.modules,
-                      location: null,
-                      userId: null,
-                      status: backupModel.status.PENDING
-                    })
-                    .then((record) => {
-                      // start the backup process
-                      // when done update backup status and file location
-                      backup.create(backupSettings.modules, backupSettings.location, (err, backupFilePath) => {
-                        let newStatus = backupModel.status.SUCCESS;
-                        if (err) {
-                          newStatus = backupModel.status.FAILED;
-                        }
-                        record.updateAttributes({status: newStatus, location: backupFilePath});
-                      });
-                    });
-                });
-                backupCron.start();
-              }
-              if (retentionIntervalDays) {
-                app.logger.debug('Scheduled automatic backup cleanup');
-
-                // schedule the backup cleanup cron
-                let backupCleanupCron = cron.schedule(`* * * ${retentionIntervalDays} * *`, function () {
-                  backup.removeBackups(new Date());
-                });
-                backupCleanupCron.start();
-              }
-            });
-          }
-        });
-    }
-  ], (err) => {
-    if (err) {
-      app.logger.error(`Failed to setup cron jobs. ${err}`);
-    }
-  });
+// function used to check if a routine should be executed or not
+// if executed return an execution time, needed for further execution
+const shouldExecute = function (startTime, interval, timeUnit) {
+  // map of time unit and moment functions to measure the duration
+  let unitsMap = {
+    h: 'hours',
+    m: 'minutes',
+    d: 'days'
+  };
+  return moment.duration(moment().diff(startTime))[unitsMap[timeUnit]]() > interval;
 };
 
+module.exports = function (app) {
+  // routines configuration file path
+  let routinesConfigFilePath = path.resolve(__dirname, '../routines.json');
 
+  // routines config
+  let routinesConfig;
+
+  // load the backup module
+  let backup = require('../../components/backup');
+
+  // routines to be executed
+  let routines = [
+    (done) => {
+      // run pre routine functionality for backup create
+      backup.preRoutine((err, backupSettings) => {
+        if (err) {
+          app.logger.warn('Failed to setup backup create job');
+          return done();
+        }
+
+        // backup interval is in hours
+        const interval = backupSettings.backupInterval;
+
+        // if intervals are 0, then don't schedule
+        if (interval < 1) {
+          // remove the old backup routine configuration
+          delete routinesConfig.backup;
+          app.logger.warn('Backup interval is less than configured threshold.');
+          return done();
+        }
+
+        // if routines configuration doesn't exist, create it
+        if (!routinesConfig.backup) {
+          routinesConfig.backup = {
+            startTime: moment(),
+            lastExecutedTime: null,
+            timeUnit: 'h',
+            interval: interval
+          };
+        }
+
+        // cache routines backup config
+        let backupRoutineConfig = routinesConfig.backup;
+
+        // if routine was executed at least once, used that date as base date for checks
+        let baseTime = backupRoutineConfig.lastExecutedTime ? backupRoutineConfig.lastExecutedTime : backupRoutineConfig.startTime;
+
+        if (shouldExecute(baseTime, backupRoutineConfig.interval, backupRoutineConfig.timeUnit)) {
+          // save the last execution time to now
+          backupRoutineConfig.lastExecutedTime = moment();
+
+          // cache backup model, used in many places below
+          const backupModel = app.models.backup;
+
+          // create new backup record with pending status
+          backupModel
+            .create({
+              date: Date.now(),
+              modules: backupSettings.modules,
+              location: null,
+              userId: null,
+              status: backupModel.status.PENDING
+            })
+            .then((record) => {
+              // start the backup process
+              // when done update backup status and file location
+              backup.create(backupSettings.modules, backupSettings.location, (err, backupFilePath) => {
+                let newStatus = backupModel.status.SUCCESS;
+                if (err) {
+                  newStatus = backupModel.status.FAILED;
+                }
+                record.updateAttributes({status: newStatus, location: backupFilePath});
+              });
+            });
+        }
+        return done();
+      });
+    },
+    (done) => {
+      // run pre routine functionality for backup cleanup
+      backup.preRoutine((err, backupSettings) => {
+        if (err) {
+          app.logger.warn('Failed to setup backup create job');
+          return done();
+        }
+
+        // backup retention interval is in days
+        const interval = backupSettings.dataRetentionInterval;
+
+        // if intervals are 0, then don't schedule
+        if (interval < 1) {
+          // remove the old backup routine configuration
+          delete routinesConfig.backup;
+          app.logger.warn('Backup retention interval is less than configured threshold.');
+          return done();
+        }
+
+        // if routines configuration doesn't exist, create it
+        if (!routinesConfig.backupCleanup) {
+          routinesConfig.backupCleanup = {
+            startTime: moment(),
+            lastExecutedTime: null,
+            timeUnit: 'd',
+            interval: interval
+          };
+        }
+
+        // cache routines backup config
+        let backupRoutineConfig = routinesConfig.backupCleanup;
+
+        // if routine was executed at least once, used that date as base date for checks
+        let baseTime = backupRoutineConfig.lastExecutedTime ? backupRoutineConfig.lastExecutedTime : backupRoutineConfig.startTime;
+
+        if (shouldExecute(baseTime, backupRoutineConfig.interval, backupRoutineConfig.timeUnit)) {
+          // save the last execution time to now
+          backupRoutineConfig.lastExecutedTime = moment();
+
+          // remove older backups
+          backup.removeBackups(new Date());
+        }
+        return done();
+      });
+    }
+  ];
+
+  // endless loop, running every 2 minutes
+  setInterval(() => {
+    try {
+      routinesConfig = JSON.parse(fs.readFileSync(routinesConfigFilePath));
+
+      // run the configured routines
+      async.parallel(
+        routines,
+        () => {
+          // write the routines config back to file
+          // to make sure any changes are persistent from tick to tick
+          try {
+            fs.writeFileSync(routinesConfigFilePath, JSON.stringify(routinesConfig));
+          } catch (writeErr) {
+            app.logger.warn(`Failed to write routines configuration. ${writeErr}`);
+          }
+        }
+      );
+    } catch (readErr) {
+      app.logger.error(`Failed to read routines configuration. ${readErr}`);
+    }
+  }, 120000);
+};
