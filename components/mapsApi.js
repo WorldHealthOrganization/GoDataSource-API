cdd375f1a38f40db54e466fda4cfc551fdef3485
Stoian Marius <stoianmarius@live.com>
2018-09-12 20:15:56 +0300

    WGD-917 Geo Location support

---
+++ components/mapsApi.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// requires
+const app = require('../server/server');
+const googleMapsService = require('@google/maps');
+
+// external API client used to retrieve the geo locations
+let client = null;
+
+// flag that indicates the client has been initialized
+// this is not enabled if the api key is not present in the config
+let isEnabled = false;
+
+/**
+ * Find geo location for an address string
+ * Using google's api
+ * @param address
+ * @param callback
+ */
+const getGeoLocation = function (address, callback) {
+  return client.geocode({
+    address: address
+  }, (err, response) => {
+    if (err) {
+      app.logger.error(`Failed to retrieve geo location for address: ${address}`);
+      return callback(err);
+    }
+
+    return callback(response.json.results.shift().geometry.location);
+  });
+};
+
+/**
+ * Initialize the external API client
+ * @param apiKey
+ */
+const initClient = function (apiKey) {
+  if (apiKey) {
+    client = googleMapsService.createClient({ key: apiKey });
+    isEnabled = true;
+  }
+
+  module.exports.isEnabled = isEnabled;
+};
+
+module.exports = {
+  getGeoLocation: getGeoLocation,
+  initClient: initClient
+};
--- server/models/person.js
+++ server/models/person.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// requires
+const _ = require('lodash');
+const async = require('async');
+const mapsApi = require('../../components/mapsApi');
+
 module.exports = function (Person) {
 
   Person.hasController = false;
@@ -129,4 +134,131 @@ module.exports = function (Person) {
       }, '');
     }
   };
+
+  // helper function used to update a person's address geo location based on city/coutnry/adress lines
+  // it queries the maps service to get the actual locations on the map
+  // then updates the record
+  const updateGeoLocations = function (personId, addresses) {
+    // index is important, for update operation
+    let addressLines = addresses.map((addr) => {
+      if (!addr) {
+        return null;
+      }
+      return `${addr.addressLine1}, ${addr.addressLine2}, ${addr.city}, ${addr.country}, ${addr.country}`;
+    });
+
+    // retrieve geo location for each address string
+    // then populate the address geo location
+    async.parallel(
+      addressLines.map((str) => {
+        return (done) => {
+          if (!str) {
+            return done(null, true);
+          }
+          if (!mapsApi.isEnabled) {
+            return done();
+          } else {
+            mapsApi.getGeoLocation(str, (err, location) => {
+              if (err) {
+                // error is logged inside the fn
+                return done();
+              }
+              return done(null, { lat: location.lat, lng: location.lng });
+            });
+          }
+        };
+      }),
+      (err, locations) => {
+        Person.findById(personId, (err, instance) => {
+          let addressCopy = instance.addresses.map((item, idx) => {
+            item = item.toObject();
+
+            // this should be left unchanged
+            // geo location was filled manually
+            if (locations[idx] === true) {
+              return item;
+            }
+
+            locations[idx] = locations[idx] || { lat: 0, lng: 0 };
+            item.geoLocation = locations[idx];
+            return item;
+          });
+
+          // update addresses array
+          instance.updateAttribute('addresses', addressCopy);
+        });
+      }
+    );
+  };
+
+  /**
+   * Used to cache the old addresses of an instance
+   * To know if the fields of interest required to generate geo location has been changed
+   */
+  Person.observe('before save', function (ctx, next) {
+    if (ctx.isNewInstance) {
+      return next();
+    }
+
+    // cache current instance addresses on ctx, needed on after save
+    ctx.options.oldAddresses = ctx.currentInstance.addresses;
+
+    return next();
+  });
+
+  /**
+   * If address is present in the request, make sure we're getting its geo location from external API
+   * We only do this if googleApi.apiKey is present in the config
+   */
+  Person.observe('after save', function (ctx, next) {
+    // cache instance reference, used in many places below
+    let instance = ctx.instance;
+
+    if (ctx.isNewInstance) {
+      // defensive checks
+      if (Array.isArray(instance.addresses)) {
+        // set address items that have geo location as undefined
+        // to not be taken into consideration
+        // i can't filter those out, because i'm losing the index of the address
+        let filteredAddresses = instance.addresses.map((addr) => addr.geoLocation ? null : addr);
+
+        // update geo locations, do not check anything
+        updateGeoLocations(instance.id, filteredAddresses);
+      }
+
+      // do not wait for the above operation to stop
+      return next();
+    }
+
+    // check if the properties for interest has been modified
+    let propsOfInterest = ['addressLine1', 'addressLine2', 'city', 'country', 'postalCode', 'geoLocation'];
+    let oldAddresses = ctx.options.oldAddresses;
+    if (oldAddresses) {
+      if (!oldAddresses.length) {
+        updateGeoLocations(instance.id, instance.addresses.map((addr) => addr.geoLocation ? addr : null));
+      } else {
+        // find the elements that match all the desired properties
+        // if it is a match, do not generate geo location
+        let filteredAddresses = instance.addresses.map((addr) => {
+          let matchedAddresses = oldAddresses
+            .filter((oldAddr) => propsOfInterest.every((prop) => {
+              if (prop === 'geoLocation') {
+                return oldAddr.geoLocation[0] === addr.geoLocation.lat &&
+                  oldAddr.geoLocation[1] === addr.geoLocation.lng;
+              }
+              return oldAddr[prop] === addr[prop];
+            }));
+
+          if (matchedAddresses.length) {
+            return null;
+          }
+
+          return addr;
+        });
+        updateGeoLocations(instance.id, filteredAddresses);
+      }
+    }
+
+    return next();
+  });
 };
