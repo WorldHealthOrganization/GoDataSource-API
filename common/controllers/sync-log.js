b7c65d9be57ddc232cbf3e20f4ad0c5296a5b98d
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-10-16 13:37:50 +0300

    WGD-1135 Filtering sync logs by syncServerUrl field doesn't work

--- common/controllers/sync-log.js
+++ common/controllers/sync-log.js
@@ -8,4 +8,31 @@ module.exports = function (SyncLog) {
     'create',
     'prototype.patchAttributes'
   ]);
+
+  /**
+   * Bulk delete sync log entries
+   * @param where
+   * @param options
+   * @param callback
+   */
+  SyncLog.bulkDelete = function (where, options, callback) {
+    // find sync log entries that need to be deleted (delete them one by one, to allow our hooks to be triggered correctly)
+    SyncLog
+      .find({
+        where: where
+      })
+      .then(function (syncLogList) {
+        // build a list of delete promises
+        const deletePromises = [];
+        // delete each sync log entry found
+        syncLogList.forEach(function (syncLog) {
+          deletePromises.push(syncLog.destroy(options));
+        });
+        return Promise.all(deletePromises);
+      })
+      .then(function (deletedRecords) {
+        callback(null, deletedRecords.length);
+      })
+      .catch(callback);
+  };
 };
