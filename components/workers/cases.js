2c08fc1f86cb7f1011c3129f77aa9163a09e56ef
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-10-26 15:08:24 +0300

    WGD-1185 Epi curve on the dashboard: case counts over time stratified by classification - the calculations need to use date of onset

--- components/workers/cases.js
+++ components/workers/cases.js
@@ -14,49 +14,19 @@ const worker = {
   countStratifiedByClassificationOverTime: function (cases, periodInterval, periodType, periodMap) {
     // go through all the cases
     cases.forEach(function (caseRecord) {
-      // keep a map of case classification history
-      const caseRecordClassificationHistoryMap = {};
-      // go through all case classifications
-      Array.isArray(caseRecord.classificationHistory) && caseRecord.classificationHistory
-        .forEach(function (classificationHistoryEntry) {
-          // make sure they all have end dates
-          if (!classificationHistoryEntry.endDate) {
-            classificationHistoryEntry.endDate = new Date();
-          }
-          // build a classification history map for each classification history entry
-          const classificationHistoryEntryMap = helpers.getChunksForInterval([
-            classificationHistoryEntry.startDate,
-            classificationHistoryEntry.endDate
-          ], periodType);
-
-          // go through the build history map
-          Object.keys(classificationHistoryEntryMap).forEach(function (index) {
-            // get correct start/end dates for period type
-            const start = helpers.getPeriodIntervalForDate(periodInterval, periodType, classificationHistoryEntryMap[index].start).shift();
-            const end = helpers.getPeriodIntervalForDate(periodInterval, periodType, classificationHistoryEntryMap[index].end).pop();
-            // add (indexed by period type) for case classification
-            caseRecordClassificationHistoryMap[`${start} - ${end}`] = classificationHistoryEntry.classification;
-          });
-        });
-      // go through the case classification history map
-      Object.keys(caseRecordClassificationHistoryMap)
-        .forEach(function (index) {
-          // if the period map entry is missing, add it
-          if (!periodMap[index]) {
-            periodMap[index] = {
-              classification: {},
-              total: 0
-            };
-          }
-          // if the period map classification entry is missing, add it
-          if (!periodMap[index].classification[caseRecordClassificationHistoryMap[index]]) {
-            periodMap[index].classification[caseRecordClassificationHistoryMap[index]] = 0;
-          }
-          // increment classification counter for period
-          periodMap[index].classification[caseRecordClassificationHistoryMap[index]]++;
-          // increment total counter per period
-          periodMap[index].total++;
-        });
+      // get interval based on date of onset
+      const caseOnsetInterval = helpers.getPeriodIntervalForDate(periodInterval, periodType, caseRecord.dateOfOnset);
+      // build period map index for onset interval
+      const periodMapIndex = `${caseOnsetInterval[0]} - ${caseOnsetInterval[1]}`;
+      // check if the classification exists on the interval
+      if (!periodMap[periodMapIndex].classification[caseRecord.classification]) {
+        // if it does not, add it
+        periodMap[periodMapIndex].classification[caseRecord.classification] = 0;
+      }
+      // increment classification counter for period
+      periodMap[periodMapIndex].classification[caseRecord.classification]++;
+      // increment total counter per period
+      periodMap[periodMapIndex].total++;
     });
     return periodMap;
   }
