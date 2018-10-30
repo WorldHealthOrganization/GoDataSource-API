ad59b54d4f29a4178436c1d3ac676647fd33ee85
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-10-30 10:24:04 +0200

    WGD-1241 EPI Curve: Error if there are cases with date of onset before outbreak start date

--- components/workers/cases.js
+++ components/workers/cases.js
@@ -9,15 +9,26 @@ const worker = {
    * @param periodInterval
    * @param periodType
    * @param periodMap
+   * @param caseClassifications
    * @return {*}
    */
-  countStratifiedByClassificationOverTime: function (cases, periodInterval, periodType, periodMap) {
+  countStratifiedByClassificationOverTime: function (cases, periodInterval, periodType, periodMap, caseClassifications) {
     // go through all the cases
     cases.forEach(function (caseRecord) {
       // get interval based on date of onset
       const caseOnsetInterval = helpers.getPeriodIntervalForDate(periodInterval, periodType, caseRecord.dateOfOnset);
       // build period map index for onset interval
       const periodMapIndex = `${caseOnsetInterval[0]} - ${caseOnsetInterval[1]}`;
+      // if an index was found outside built periodMap (this usually happens when dateOfOnset is before outbreak startDate)
+      if (!periodMap[periodMapIndex]) {
+        // init periodMap entry
+        periodMap[periodMapIndex] = {
+          start: caseOnsetInterval[0],
+          end: caseOnsetInterval[1],
+          classification: Object.assign({}, caseClassifications),
+          total: 0
+        };
+      }
       // check if the classification exists on the interval
       if (!periodMap[periodMapIndex].classification[caseRecord.classification]) {
         // if it does not, add it
