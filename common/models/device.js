a0b7fd6f2835b4a2ecc8e762b9c52b5e07ba7ec0
Adrian Dobre <adrian.dobre@clarisoft.com>
2018-11-01 15:50:31 +0200

    WGD-540 ST1: I want to be able to remotely wipe data from an iOS or Android device

--- common/models/device.js
+++ common/models/device.js
@@ -1,12 +1,19 @@
 'use strict';
 
+const app = require('../../server/server');
+
 module.exports = function (Device) {
   Device.statusList = {
     wipeReady: 'LNG_DEVICE_WIPE_STATUS_READY',
     wipePending: 'LNG_DEVICE_WIPE_STATUS_PENDING'
   };
 
+  /**
+   * Send device wipe request
+   * @return {*|Promise}
+   */
   Device.prototype.sendWipeRequest = function () {
-    return Promise.resolve('aaaa');
+    return app.utils.services.pushNotificationsApi
+      .sendWipeRequest(this.physicalDeviceId);
   };
 };
