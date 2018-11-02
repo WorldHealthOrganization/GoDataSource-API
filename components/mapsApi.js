044d660fe8ced042da1d12a8de0e0aadc5fc74c6
Stoian Marius <stoianmarius@live.com>
2018-11-02 14:20:21 +0200

    WGD-1154 Replace Google Maps with something else to plot Chains of Transmission on a map

--- components/mapsApi.js
+++ components/mapsApi.js
@@ -2,15 +2,20 @@
 
 // requires
 const app = require('../server/server');
-const googleMapsService = require('@google/maps');
+const appConfig = require('../server/config.json');
+const request = require('request');
 const _ = require('lodash');
 
-// external API client used to retrieve the geo locations
-let client = null;
+// external service base URL
+// TODO: subject to change, as authentication and geocode server could have different addresses
+const baseURL = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer';
 
-// flag that indicates the client has been initialized
-// this is not enabled if the api key is not present in the config
-let isEnabled = false;
+// token expiration time set to maximum available
+// 14 days in minutes (that is the external service format)
+const tokenExpirationInMinutes = 20160;
+
+// generated token and expiration time
+let token = null;
 
 /**
  * Find geo location for an address string
@@ -19,36 +24,95 @@ let isEnabled = false;
  * @param callback
  */
 const getGeoLocation = function (address, callback) {
-  try {
-    client.geocode({
-      address: address
-    }, function (err, response) {
-      if (err) {
-        app.logger.error(`Failed to retrieve geo location for address: ${address}. API response: ${err}`);
-        return callback(err);
-      }
+  let getLocation = function () {
+    return request.get(
+      `${baseURL}/findAddressCandidates?SingleLine=${address}&category=&outFields=*&forStorage=false&f=json&token=${token}`,
+      (err, response, responseBody) => {
+        if (err) {
+          app.logger.warn(`Failed to generate geocode. ${err}`);
+          return callback(err);
+        }
 
-      return callback(null, _.get(response.json.results.shift(), 'geometry.location'));
-    });
-  } catch (err) {
-    // when the API key is invalid, the library is throwing an error
-    return callback(err);
-  }
+        if (responseBody) {
+          if (responseBody.error) {
+            // invalid token
+            if (responseBody.error.code === 498) {
+              return generateAccessToken((err) => {
+                if (err) {
+                  return callback(err);
+                }
+
+                // token generation failed
+                // nothing to do
+                if (!token) {
+                  return callback();
+                }
+
+                // retry the request
+                return getLocation();
+              });
+            }
+
+            // unexpected error, get out
+            return callback();
+          }
+
+          // select the address candidate with highest score
+          // return its coordinates
+          let bestCandidate = _.maxBy(responseBody.candidates, (candidate) => candidate.score);
+
+          return callback(null, bestCandidate ? bestCandidate.location : null);
+        }
+      }
+    );
+  };
 };
 
 /**
- * Initialize the external API client
- * @param apiKey
+ * Generate a new access token for using the external maps service
+ * Information about the token are stored inside the module
+ * Credentials are token from application config
  */
-const initClient = function (apiKey) {
-  if (apiKey) {
-    client = googleMapsService.createClient({ key: apiKey });
-    isEnabled = true;
-  }
+const generateAccessToken = function (cb) {
+  let mapsOpts = appConfig.mapsApi;
+
+  return request.get(
+    `https://www.arcgis.com/sharing/oauth2/token?
+      client_id=${mapsOpts.clientId}&
+      grant_type=client_credentials&
+      client_secret=${mapsOpts.clientSecret}&
+      expiration=${tokenExpirationInMinutes}&
+      f=json
+    `,
+    (err, response, responseBody) => {
+      if (err) {
+        app.logger.warn(`Failed to generate access token for maps API. ${err}`);
+        return callback(err);
+      }
+
+      if (responseBody) {
+        if (responseBody.error) {
+          app.logger.warn(`Failed to generate access token for maps API. ${responseBody}`);
+          return callback();
+        }
+        token = responseBody.access_token;
+      }
+
+      if (cb) {
+        return cb();
+      }
+    }
+  );
+};
+
+/**
+ * Initialize the module by generating authenticating the client and storing information about the token
+ */
+const initClient = function () {
+  generateAccessToken();
 };
 
 module.exports = {
   getGeoLocation: getGeoLocation,
-  initClient: initClient,
-  isEnabled: () => isEnabled
+  initClient: initClient
 };
--- server/boot/mapsApi.js
+++ server/boot/mapsApi.js
@@ -4,12 +4,5 @@
 const _ = require('lodash');
 
 module.exports = function () {
-  // retrieve app's config and try to find the google's api key
-  const appConfig = require('../config.json');
-
-  // try to get reference to the api key
-  let apiKey = _.get(appConfig, 'googleApi.apiKey');
-
-  // if API key is not set, the component will not boot
-  require('../../components/mapsApi').initClient(apiKey);
+  require('../../components/mapsApi').initClient();
 };
