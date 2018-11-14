9fedc7de37507042a5a66edc277e0fb4e1ff3c8e
Stoian Marius <stoianmarius@live.com>
2018-11-14 11:12:37 +0200

    WGD-1154 Replace Google Maps with something else to plot Chains of Transmission on a map

--- common/controllers/maps.js
+++ common/controllers/maps.js
@@ -2,7 +2,6 @@
 
 // requires
 const app = require('../../server/server');
-const request = require('request');
 const _ = require('lodash');
 const mapsApi = require('../../components/mapsApi');
 const appConfig = require('../../server/config.json');
@@ -17,38 +16,6 @@ module.exports = function (Maps) {
     'count',
     'find'
   ]);
-
-  /**
-   * Retrieve Google API js script and serve it as reponse
-   * @param callback
-   */
-  Maps.getAPIScript = function (callback) {
-    // try to get reference to the api key
-    let apiKey = _.get(appConfig, 'googleApi.apiKey');
-
-    // stop with not found
-    if (!apiKey) {
-      return callback(app.utils.apiError.getError('MAPS_API_KEY_INVALID'));
-    }
-
-    // if the script is already stored into memory just return it
-    // otherwise, attach the key query param to the configured script path and store & return it
-    if (Maps.scriptBuffer) {
-      return callback(null, Maps.scriptBuffer);
-    }
-
-    request
-      .get(`${Maps.scriptPath}${apiKey}`, (err, res, body) => {
-        if (err) {
-          return callback(err);
-        }
-
-        // store it into memory
-        Maps.scriptBuffer = Buffer.from(body);
-
-        return callback(null, Maps.scriptBuffer);
-      });
-  };
 
   /**
    * Find Geo-Location for an address
