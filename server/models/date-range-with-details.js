da0eebd6723008b8d02bf2e721d58421e837c15b
adrian-toader <adrian.toader@clarisoft.com>
2019-01-07 14:45:58 +0200

    WGD-1718 Additional fields for hospitalization / isolation blocks

--- server/models/date-range-with-details.js
+++ server/models/date-range-with-details.js
@@ -5,7 +5,7 @@ module.exports = function (DateRange) {
   DateRange.fieldLabelsMap = {
     'startDate': 'LNG_OUTBREAK_FIELD_LABEL_START_DATE',
     'endDate': 'LNG_OUTBREAK_FIELD_LABEL_END_DATE',
-    'hospitalName': 'LNG_CASE_FIELD_LABEL_DATE_RANGE_HOSPITAL_NAME',
+    'centerName': 'LNG_CASE_FIELD_LABEL_DATE_RANGE_CENTER_NAME',
     'locationId': 'LNG_CASE_FIELD_LABEL_DATE_RANGE_LOCATION',
     'comments': 'LNG_CASE_FIELD_LABEL_DATE_RANGE_COMMENTS'
   };
@@ -13,7 +13,7 @@ module.exports = function (DateRange) {
   DateRange.printFieldsinOrder = [
     'startDate',
     'endDate',
-    'hospitalName',
+    'centerName',
     'locationId',
     'comments'
   ];
