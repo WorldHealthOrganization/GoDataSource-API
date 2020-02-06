00e9a74e8f51899d12a111be1bb0fba21e5d2411
st0ian <marius.stoian@clarisoft.com>
2020-02-06 10:51:13 +0200

    WGD-2764

--- server/boot/cors.js
+++ server/boot/cors.js
@@ -3,18 +3,19 @@
 const cors = require('cors');
 
 module.exports = function (app) {
+  const corsOptions = {
+    origin: '*',
+    credentials: true
+  };
   if (app.settings.cors && app.settings.cors.enabled) {
-    const corsOptions = {
-      credentials: true,
-      origin: function (origin, callback) {
-        // !origin allow server-to-server requests
-        if ((app.settings.cors.whitelist || []).indexOf(origin) !== -1 || !origin) {
-          return callback(null, true);
-        } else {
-          return callback(new Error('Not allowed by CORS'));
-        }
+    corsOptions.origin = function (origin, callback) {
+      // !origin allow server-to-server requests
+      if ((app.settings.cors.whitelist || []).indexOf(origin) !== -1 || !origin) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
       }
     };
-    app.middleware('initial', cors(corsOptions));
   }
+  app.middleware('initial', cors(corsOptions));
 };
