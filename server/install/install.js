a33a2106c9e631b3ca97acea5673a8c511655866
adrian-toader <adrian.toader@clarisoft.com>
2020-04-02 10:52:06 +0300

    WGD-2977 Implement the means to perform stress test on Go.Data's systems

--- server/install/install.js
+++ server/install/install.js
@@ -222,6 +222,7 @@ const routines = {
       'subLocationsLevelsNo',
       'minNoRelationshipsForEachRecord',
       'maxNoRelationshipsForEachRecord',
+      'relationshipsForAlreadyAssociatedPerson',
       'batchSize'
     ];
 
