eb3e6403dcd4e1ab2b5958013b4fa77973ab5746
Adrian Morariu <adrian.morariu@clarisoft.com>
2021-08-05 19:33:05 +0300

    WGD-3579 [Service] Testing on whohuge-1m

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -22,7 +22,7 @@ const migrationVersions = [{
     fileName: 'defaultLanguages.js',
     actions: [{
       name: 'run',
-      buildNo: 23
+      buildNo: 24
     }]
   }, {
     fileName: 'defaultReferenceData.js',
