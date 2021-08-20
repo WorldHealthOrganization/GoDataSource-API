5b78f593cbe58786f2fbc9b5cef4f315d7e140f9
adrian.toader <adrian.toader@clarisoft.com>
2021-08-20 17:48:56 +0300

    WGD-3695 Upgrade "sharp" library

--- components/workers/helpers.js
+++ components/workers/helpers.js
@@ -46,34 +46,6 @@ const worker = {
    */
   decrypt: aesCrypto.decrypt,
   /**
-   * Create a PDF file containing PNG images
-   * @param imageData
-   * @param splitFactor Split the image into:
-   * - a nxm matrix computed based on the provided image size
-   * - a square matrix with a side of <splitFactor> (1 no split, 2 => 2x2 grid, 3 => 3x3 grid) when splitType is grid
-   * - a list of <splitFactor> images, divided horizontally when splitType is horizontal
-   * - a list of <splitFactor> images, divided vertically when splitType is vertical
-   * @param splitType enum: ['auto', grid', 'horizontal', 'vertical']. Default 'auto'.
-   * @param callback
-   */
-  createImageDoc: function (imageData, splitFactor, splitType) {
-    return new Promise(function (resolve, reject) {
-      // transform data back to buffer
-      if (
-        imageData &&
-        imageData.data &&
-        imageData.type === 'Buffer') {
-        imageData = Buffer.from(imageData.data);
-      }
-      pdfDoc.createImageDoc(imageData, splitFactor, splitType, function (error, result) {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(result);
-      });
-    });
-  },
-  /**
    * Export a filtered list of models
    */
   exportFilteredModelsList: exportHelper.exportFilteredModelsList
