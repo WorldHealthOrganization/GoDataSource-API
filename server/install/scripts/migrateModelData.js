4c7845958d4b57f76babc2c63a7056d5eb861e95
Adrian Morariu <adrian.morariu@clarisoft.com>
2021-12-13 11:57:04 +0200

    WGD-3767 Prompt if a contact with the same name as a contact exists

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -190,7 +190,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 10
+      buildNo: 11
     }]
   }, {
     fileName: 'person.js',
