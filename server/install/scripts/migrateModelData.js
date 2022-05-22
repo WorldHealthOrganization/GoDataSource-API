61af1d11d21d70b0f8ea39ff8d43d1ad532127cc
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-05-22 12:20:36 +0300

    WGD-3567 PRIORITY: UI/UX Improvement

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -235,7 +235,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 18
+      buildNo: 19
     }]
   }]
 }];
