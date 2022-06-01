642978956334923aa7329d413fe1d9a0df553d38
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-06-01 09:44:27 +0300

    WGD-3902 Update several French language tokens

--- server/install/scripts/migrations/2.41.0/languageToken.js
+++ server/install/scripts/migrations/2.41.0/languageToken.js
@@ -14,7 +14,24 @@ const createUpdateLanguageTokens = (callback) => {
     .catch(callback);
 };
 
+/**
+ * Create / Update only french language tokens
+ */
+const createUpdateSingleFrenchLanguageTokens = (callback) => {
+  languageMigrator
+    .createUpdateLanguageTokens(
+      `${__dirname}/data/french_single`, [
+        'french_fr'
+      ]
+    )
+    .then(() => {
+      callback();
+    })
+    .catch(callback);
+};
+
 // export list of migration jobs; functions that receive a callback
 module.exports = {
-  createUpdateLanguageTokens
+  createUpdateLanguageTokens,
+  createUpdateSingleFrenchLanguageTokens
 };
