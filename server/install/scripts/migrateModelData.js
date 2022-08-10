0c0bde03170c31d45bb2f5b05ef587b05a55dc57
Adrian Morariu <adrian.morariu@clarisoft.com>
2022-08-10 14:55:42 +0300

    Merge web tokens

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -268,7 +268,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 1
+      buildNo: 2
     }]
   }, {
     fileName: 'outbreak.js',
