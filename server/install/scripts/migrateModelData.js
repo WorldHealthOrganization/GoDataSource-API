c51fc5699b44ec5a6e5be6326ece018b4c69ae99
Adrian Morariu <adrian.morariu@clarisoft.com>
2023-02-16 10:54:22 +0200

    WGD-3871 Option to delete contact and its follow-ups at the same time

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -322,7 +322,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 32
+      buildNo: 35
     }]
   }]
 }];
