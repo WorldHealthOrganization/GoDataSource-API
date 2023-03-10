e58f98a7f9e6726da524ab051a608e958c9e2a37
Valeriu Onofrei <valeriu.onofrei@clarisoft.com>
2023-03-10 17:41:49 +0200

    WGD-4135 SVD feedback - Consider include all variables in the export file in the Daily follow-up dashboard

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -343,7 +343,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 12
+      buildNo: 14
     }]
   }]
 }];
