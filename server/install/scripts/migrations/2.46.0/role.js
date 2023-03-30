02bb9a825131c9ad879ea1aa092640cb8966c7b3
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-03-30 14:01:30 +0300

    WGD-4029 Add Event ID field

--- server/install/scripts/migrations/2.46.0/role.js
+++ server/install/scripts/migrations/2.46.0/role.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const MongoDBHelper = require('../../../../../components/mongoDBHelper');
+const Helpers = require('../../../../../components/helpers');
+
+// Number of find requests at the same time
+// Don't set this value to high so we don't exceed Mongo 16MB limit
+const findBatchSize = 1000;
+
+// set how many item update actions to run in parallel
+const updateBatchSize = 10;
 
 /**
  * Add event_generate_visual_id permission for the following permission: event_create, event_modify and event_import
@@ -12,74 +20,86 @@ const addMissingPermission = (callback) => {
   const PERMISSION_EVENT_IMPORT = 'event_import';
   const PERMISSION_EVENT_GENERATE_VISUAL_ID = 'event_generate_visual_id';
 
+  let roleCollection;
+
   // create Mongo DB connection
   return MongoDBHelper
     .getMongoDBConnection()
-    .then((dbConn) => {
+    .then(dbConn => {
       // collections
-      const roleCollection = dbConn.collection('role');
+      roleCollection = dbConn.collection('role');
 
-      // add missing permission to required roles
-      return roleCollection
-        .find({
-          $and: [
-            {
-              'permissionIds': {
-                $in: [
-                  PERMISSION_EVENT_CREATE,
-                  PERMISSION_EVENT_MODIFY,
-                  PERMISSION_EVENT_IMPORT
-                ]
-              }
-            }, {
-              'permissionIds': {
-                $nin: [
-                  PERMISSION_EVENT_GENERATE_VISUAL_ID
-                ]
-              }
+      // initialize parameters for handleActionsInBatches call
+      const roleQuery = {
+        $and: [
+          {
+            'permissionIds': {
+              $in: [
+                PERMISSION_EVENT_CREATE,
+                PERMISSION_EVENT_MODIFY,
+                PERMISSION_EVENT_IMPORT
+              ]
             }
-          ]
-        }, {
-          projection: {
-            _id: 1
+          }, {
+            'permissionIds': {
+              $nin: [
+                PERMISSION_EVENT_GENERATE_VISUAL_ID
+              ]
+            }
           }
-        })
-        .toArray()
-        .then((results) => {
-          if (results.length < 1) {
-            console.log('No role found to update');
-            return callback();
-          } else {
-            const roleIds = [];
-            results.forEach((result) => {
-              roleIds.push(result['_id']);
-            });
+        ]
+      };
 
-            // log
-            console.log('The following roles will be updated: ' + roleIds);
+      // initialize parameters for handleActionsInBatches call
+      const getActionsCount = () => {
+        // count roles
+        return roleCollection
+          .countDocuments(roleQuery);
+      };
 
-            // add missing permission to resulted roles
-            return roleCollection
-              .updateMany({
-                _id: {
-                  $in: roleIds
-                }
-              }, {
-                '$push': {
-                  permissionIds: PERMISSION_EVENT_GENERATE_VISUAL_ID
-                }
-              })
-              .then(() => {
-                // log
-                console.log('The roles were successfully updated');
+      // get records in batches
+      const getBatchData = (batchNo, batchSize) => {
+        // get records for batch
+        return roleCollection
+          .find(roleQuery, {
+            skip: 0,
+            limit: batchSize,
+            projection: {
+              _id: 1
+            }
+          })
+          .toArray();
+      };
 
-                // finished
-                callback();
-              })
-              .catch(callback);
-          }
-        })
-        .catch(callback);
+      // update records
+      const itemAction = (data) => {
+        // log
+        console.log('The missing role will be added in the following role: ' + data._id);
+
+        // add missing permission to resulted roles
+        return roleCollection
+          .updateMany({
+            _id: data._id
+          }, {
+            '$push': {
+              permissionIds: PERMISSION_EVENT_GENERATE_VISUAL_ID
+            }
+          });
+      };
+
+      // execute
+      return Helpers.handleActionsInBatches(
+        getActionsCount,
+        getBatchData,
+        null,
+        itemAction,
+        findBatchSize,
+        updateBatchSize,
+        console
+      );
+    })
+    .then(() => {
+      callback();
     })
     .catch(callback);
 };
