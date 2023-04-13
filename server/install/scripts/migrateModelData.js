13de2fec01d69646f2858acc2e6c2406f1c45f51
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-04-13 08:25:44 +0300

    WGD-4057 When generating a sync package, user importing sync package will get an error unless the user has been assigned to the outbreak that is being exported in the sync package

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -343,7 +343,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 10
+      buildNo: 11
     }]
   }, {
     fileName: 'role.js',
