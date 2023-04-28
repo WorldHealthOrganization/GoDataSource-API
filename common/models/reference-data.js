5d3af056b2327a4c1a218a4605b2e95e33950339
Adrian Morariu <adrian.morariu@clarisoft.com>
2023-04-28 17:28:38 +0300

    WGD-3498 PRIORITY: Reference data should be linked to the disease chosen for the outbreak

--- common/models/reference-data.js
+++ common/models/reference-data.js
@@ -688,7 +688,7 @@ module.exports = function (ReferenceData) {
                 }
 
                 // allow customizing some safe properties
-                const customizableProperties = ['iconId', 'colorCode', 'order', 'code', 'geoLocation'];
+                const customizableProperties = ['iconId', 'colorCode', 'order', 'code', 'geoLocation', 'isSystemWide', 'allowedRefDataItems'];
 
                 // if model is editable but in use, also let it change the 'active', 'value', 'description' fields
                 if (error.code === 'MODEL_IN_USE') {
