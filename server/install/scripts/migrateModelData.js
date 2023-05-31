0d8ce657acc8f32a127ae35060d2be5fbfc94802
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-05-31 14:20:58 +0300

    WGD-3586 PRIORITY: Conversion of contacts of contacts

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -367,7 +367,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 17
+      buildNo: 18
     }, {
       name: 'createUpdateSingleEnglishLanguageTokens',
       buildNo: 1
