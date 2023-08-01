5590f8033140bd745394baf638bfe3347d46e6be
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-08-01 09:28:25 +0300

    WGD-2793 Import/export of Go.Data Users [Service]

--- common/models/user.js
+++ common/models/user.js
@@ -461,8 +461,8 @@ module.exports = function (User) {
   ];
 
   User.arrayProps = {
-    roleIds: true,
-    outbreakIds: true,
+    outbreakIds: 'LNG_USER_FIELD_LABEL_AVAILABLE_OUTBREAKS',
+    roleIds: 'LNG_USER_FIELD_LABEL_ROLES',
     securityQuestions: {
       'id': 'LNG_COMMON_MODEL_FIELD_LABEL_ID',
       'question': 'LNG_USER_FIELD_LABEL_SECURITY_QUESTIONS_QUESTION',
