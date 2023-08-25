61ffbdf7a5be710ccae77b67ba5c86fa6bd8b373
vonofrei-clarisoft <valeriu.onofrei@clarisoft.com>
2023-08-25 07:41:04 +0300

    WGD-4268 [Web + Service] Issues when contact converted to contact of contact

--- server/install/scripts/migrateModelData.js
+++ server/install/scripts/migrateModelData.js
@@ -391,7 +391,7 @@ const migrationVersions = [{
     fileName: 'languageToken.js',
     actions: [{
       name: 'createUpdateLanguageTokens',
-      buildNo: 18
+      buildNo: 19
     }]
   }, {
     fileName: 'referenceData.js',
